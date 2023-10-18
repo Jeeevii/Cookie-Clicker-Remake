014d7db1ebd8c5ff5dab2ab3fd621a244e9332e8
Jeevithan Mahenthran <127083379+Jeeevii@users.noreply.github.com>
2023-10-17 17:34:03 -0700

    finished step 1

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,38 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Jeevi's Game";
-
 document.title = gameName;
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+let cashCounter = 0;
+
+// cash button
+const cash = document.createElement("button");
+cash.innerHTML = "💵";
+cash.style.fontSize = "30pt";
+app.append(cash);
+
+// clicker text 
+
+const cashCount = document.createElement("div");
+cashCount.innerHTML = `Money Stolen: ${cashCounter}`;
+cashCount.style.fontSize = "15pt";
+app.append(cashCount);
+
+// hammer upgrade
+
+const hammer = document.createElement("button");
+hammer.innerHTML = "🔨";
+hammer.style.fontSize = "30pt";
+app.append(hammer);
+
+// clicker text incriminating
+
+cash.addEventListener("click", () => {
+    cashCounter += 1;
+    cashCount.innerHTML = `Money Stolen: ${cashCounter}`;
+    
+});
+
