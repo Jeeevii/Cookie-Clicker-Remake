245e3c15a32f3baa42fe574a99b20560a1348558
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2023-10-19 13:49:21 -0700

    step 6, step 7, step 8, step 9 and step 10

--- src/main.ts
+++ src/main.ts
@@ -8,82 +8,137 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// Initialize clicker
 let cashCounter = 0;
+// Initialize passive income
+let passiveClicks = 0;
 
-// cash button
+// Cash button setup
 const cash = document.createElement("button");
-cash.textContent = "💵";
+cash.textContent = "💵"; // Display cash symbol
 cash.style.fontSize = "25px";
 app.append(cash);
 
-// clicker text
+// Counter for money stolen
 const cashCount = document.createElement("div");
 cashCount.textContent = `Money Stolen: ${cashCounter}`;
 app.append(cashCount);
 
-// clicker text incriminating
+// Increment money when clicking the cash button
 cash.addEventListener("click", () => {
-    cashCounter += 1;
-    updateDisplay();
+  cashCounter += 1;
+  updateDisplay();
 });
 
-// hammer upgrade
-const hammer = document.createElement("button");
-hammer.textContent = "🔨";
-hammer.style.fontSize = "20px";
-hammer.disabled = true;
+// Upgrade buttons
+const hammer = createUpgradeButton(
+  "🔨",
+  "Use the hammer to break into a Car!",
+  10, // Cost
+  0.2, // Passive Clicker
+);
 app.append(hammer);
 
-// hammer description
-const hammerDes = document.createElement("div");
-hammerDes.textContent = `Use the hammer to break into a Car!`;
-app.append(hammerDes);
+const knife = createUpgradeButton(
+  "🔪",
+  "Use the knife to rob an innocent Civilian!",
+  50,
+  1,
+);
+app.append(knife);
 
-// upgrade costs
-const hammerCost = 10;
+const watergun = createUpgradeButton(
+  "🔫",
+  "Use the water gun to rob your local Dollar Tree!",
+  250,
+  5,
+);
+app.append(watergun);
 
-// upgrade effect
-let passiveClicks = 0;
+const homies = createUpgradeButton(
+  "😎",
+  "Get the homies together and loot a Store! Keep it under $950!",
+  950,
+  100,
+);
+app.append(homies);
 
-// check upgrade
+const car = createUpgradeButton(
+  "🚗",
+  "Use the stolen car to commit insurance fraud!",
+  10000,
+  1000,
+);
+app.append(car);
+
+// Function to create upgrade buttons
+function createUpgradeButton(
+  name: string,
+  description: string,
+  cost: number,
+  income: number,
+) {
+  const button = document.createElement("button");
+  button.textContent = `${name} Buy for ${cost}💵`;
+  button.style.fontSize = "20px";
+  app.append(button);
+
+  button.addEventListener("click", () => {
+    if (cashCounter >= cost) {
+      cashCounter -= cost; // Subtract cash when bought
+      passiveClicks += income; // Add the passive upgrade
+      updateDisplay();
+    }
+  });
+
+  const descriptionElement = document.createElement("div");
+  descriptionElement.textContent = `${description} (Passive Income: ${income}/s)`;
+  app.append(descriptionElement);
+
+  return button;
+}
+
+// Function to update upgrade button status
+function updateItemStatus(button: HTMLButtonElement, cost: number) {
+  if (button) {
+    if (cashCounter >= cost) {
+      button.disabled = false;
+      button.textContent = `${button.textContent?.replace("Cost", "Buy")}`;
+    } else {
+      button.disabled = true;
+      button.textContent = `${button.textContent?.replace("Buy", "Cost")}`; // Cash isn't enough to buy
+    }
+  }
+}
+
+// Check and update the status of upgrade buttons
 function updateHammer() {
-  if (cashCounter >= hammerCost) {
-    hammer.disabled = false;
-    hammer.textContent = "🔨 Buy for 10💵";
-  } else {
-    hammer.disabled = true;
-    hammer.textContent = "🔨 Cost: 10💵";
-  }
+  updateItemStatus(hammer, 10);
+  updateItemStatus(knife, 50);
+  updateItemStatus(watergun, 250);
+  updateItemStatus(homies, 950);
+  updateItemStatus(car, 10000);
 }
 
-// upgrade handler
-hammer.addEventListener("click", () => {
-  if (cashCounter >= hammerCost) {
-    cashCounter -= hammerCost; // Spend the money
-    passiveClicks += 0.5; // increase passive income from hammer
-    updateHammer();
-  }
-});
-
-// continuous counter growth using requestAnimationFrame
+// Continuous counter growth using requestAnimationFrame
 function continuousCounterGrowth() {
-  cashCounter += passiveClicks / 60; // increment by 1/60 of the passive income per frame
+  cashCounter += passiveClicks / 60;
   updateDisplay();
-  requestAnimationFrame(continuousCounterGrowth); // continue the loop
+  requestAnimationFrame(continuousCounterGrowth);
 }
 
-// start the continuous counter growth loop
+// Start the continuous counter growth loop
 continuousCounterGrowth();
 
-// display update
+// Update the display
 function updateDisplay() {
-  cashCount.textContent = `Money Stolen: ${Math.round(cashCounter)}`; // round the cashCounter for display
+  cashCount.textContent = `Money Stolen: ${Math.round(cashCounter)}`;
   updateHammer();
 }
 
-// add a setInterval for continuous counter growth
+// Add a setInterval for continuous counter growth
 setInterval(() => {
-  cashCounter += passiveClicks; // increment by the passive income per second
+  cashCounter += passiveClicks;
   updateDisplay();
 }, 1000);
 
